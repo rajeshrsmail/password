d8cf67fda3a40261e985e3aef462aedba4560272
rajeshrsmail <rajeshrsmail@gmail.com>
2011-03-12 17:32:40 +0530

    my complete script

--- masterscript.sh
+++ masterscript.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
-ssh root@192.168.1.25 'echo "root:bland" | chpasswd'
+MYPATH=/opt/iplist
+for i in `cat $MYPATH` 
+do
+echo "########################"
+echo $i 
+ssh root@$i 'echo "root:rohini" | chpasswd'
+if [ $? = 0 ];then
+echo "Root password change for $i was successful" 
+else
+echo "Root password change for $i was not successful" 
+echo 
+echo
+fi
+ssh root@$i 'echo "rajesh:rohini" | chpasswd'
+if [ $? = 0 ];then
+echo "Rajesh password change for $i was successful"
+else
+echo "Rajesh password change for $i was not successful" 
+fi
+echo
+echo
+done
 
 
